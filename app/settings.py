112602d995f1c9b3a6a6e4fa468e5cc482acfb86
raoasifraza11 <raoasifraza1@hotmail.com>
2018-09-01 16:55:30 +0500

    Fixing static paths configurations

--- app/settings.py
+++ app/settings.py
@@ -121,16 +121,22 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
 
-STATIC_URL = 'app/app/static/'
-MEDIA_URL ='app/app/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT  = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+#STATIC_ROOT  = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
+
+# FOR server
+STATIC_ROOT  = "/opt/bitnami/apps/django/django_projects/app/static"
+STATIC_URL = '/static/'
+MEDIA_URL ='/media/'
+
+
