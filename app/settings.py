cbda86b5dfd50a6032cb4e063898d18d7888d4ed
raoasifraza11 <raoasifraza1@hotmail.com>
2018-09-05 13:27:01 +0500

    Fix css accessed by aap/static

--- app/settings.py
+++ app/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static_cdn"),
 ]
 
 STATIC_ROOT  = os.path.join(BASE_DIR,"static_cdn")
