56afd3701f0898175fa8de80b6ec3a890069c9df
raoasifraza11 <raoasifraza1@hotmail.com>
2018-09-06 11:35:12 +0500

    Rewrite apache conf && added vhost

--- app/settings.py
+++ app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w%uk$3#+33fc@ei%qbkboxn@pgylx-e_z60t=3z!_ok(!(vz)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['18.185.38.39', 'abasynisb.edu.pk', "www.abasynisb.edu.pk"]
 
