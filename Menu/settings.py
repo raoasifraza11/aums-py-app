0017c29d1e58740db40a8c07e68a93357a44b007
raoasifraza11 <raoasifraza1@hotmail.com>
2018-10-16 12:06:03 +0500

    Added sqlite path

--- Menu/settings.py
+++ Menu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w%uk$3#+33fc@ei%qbkboxn@pgylx-e_z60t=3z!_ok(!(vz)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 'www.abasynisb.edu.pk',
