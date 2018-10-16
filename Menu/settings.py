64d24b58a6aa9abc3a7b06ab1a5c48e483924104
raoasifraza11 <raoasifraza1@hotmail.com>
2018-10-16 12:02:11 +0500

    Added sqlite path

--- Menu/settings.py
+++ Menu/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'Menu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/bitnami/apps/django/django_projects/app/db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
