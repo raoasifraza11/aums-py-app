20c86069ed147cdaf5cd1db6e12968b3b6e7f19f
raoasifraza11 <raoasifraza1@hotmail.com>
2018-10-18 10:55:48 +0500

    Added setting db

--- Menu/settings.py
+++ Menu/settings.py
@@ -93,10 +93,15 @@ WSGI_APPLICATION = 'Menu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'addata',
+        'USER': 'root',
+        'PASSWORD': 'K1HR2fcdWuW8',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
