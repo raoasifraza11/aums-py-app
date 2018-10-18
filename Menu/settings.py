da17d6d1d2ec7ca9539de85a027aa57344b2a26e
raoasifraza11 <raoasifraza1@hotmail.com>
2018-10-18 11:33:46 +0500

    Revert changes

--- Menu/settings.py
+++ Menu/settings.py
@@ -93,8 +93,14 @@ WSGI_APPLICATION = 'Menu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'addata',
@@ -103,7 +109,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
-}
+}'''
 
 
 # Password validation
