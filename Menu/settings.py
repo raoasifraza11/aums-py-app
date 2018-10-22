331a5362e695a6ceb8aedb69530c2b0b43423195
raoasifraza11 <raoasifraza1@hotmail.com>
2018-10-22 13:55:44 +0500

    Added config

--- Menu/settings.py
+++ Menu/settings.py
@@ -93,14 +93,14 @@ WSGI_APPLICATION = 'Menu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'addata',
@@ -109,7 +109,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
-}'''
+}
 
 
 # Password validation
