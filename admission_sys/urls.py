cdb90796491593445d0a448404ee3c324ff09d02
ahmadkhan2208 <ahmad.ali@abasynisb.edu.pk>
2019-06-28 01:38:36 +0500

    remove auth views

--- admission_sys/urls.py
+++ admission_sys/urls.py
@@ -3,14 +3,9 @@ from django.conf.urls import url
 from . import views as vs
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
     path('',vs.login_user , name = 'login'),
     path('signup/',vs.signup , name = 'signup'),
     path('application/',vs.application,name = 'application'),
