c1342a6ba5d97eb95107182a24a4597faa699e13
ahmadkhan2208 <ahmad.ali@abasynisb.edu.pk>
2019-10-27 01:22:42 -0700

    activating ansec

--- Menu/urls.py
+++ Menu/urls.py
@@ -8,7 +8,7 @@ from django.urls import path
 urlpatterns = [
     url(r'^', include('slider.urls')),
     #url(r'^alumni', include('alumni.urls')),
-    #url(r'^ansec', include('Ansec.urls')),
+    url(r'^ansec', include('Ansec.urls')),
     url(r'^admin/', admin.site.urls),
     path('Apply_Online/', include('admission_sys.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
